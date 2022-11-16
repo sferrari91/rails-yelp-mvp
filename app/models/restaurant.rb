6ccfe4a914eeb178f1a785d1d4db6b36ea1025df
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2022-11-16 11:42:34 +0100

    validations done

---
+++ app/models/restaurant.rb
@@ -0,0 +1,14 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+
+end
+
+
+
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.
---
+++ db/migrate/20221116095510_create_restaurants.rb
@@ -0,0 +1,17 @@
+class CreateRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
+
+
+# REMOVE PHONE AND ADD(RAILS GENERATE MIGRATION)
+
+# DB DROP AND DB CREATE DB MIGRATE
